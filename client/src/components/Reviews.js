a68df3509b55b1eb55877eedf85af0caf0d0ac8c
abhishek1901ee04 <54908876+abhishek1901ee04@users.noreply.github.com>
2022-10-20 01:25:49 +0530

    pushing frontend

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -1,74 +1,86 @@
-import React, { useState, useContext } from "react";
-import RestaurantFinder from "../apis/RestaurantFinder";
-import { RestaurantsContext } from "../context/RestaurantsContext";
+import React from "react";
+import StarRating from "./StarRating";
 
-const AddRestaurant = () => {
-  const { addRestaurants } = useContext(RestaurantsContext);
-  const [name, setName] = useState("");
-  const [location, setLocation] = useState("");
-  const [priceRange, setPriceRange] = useState("Price Range");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await RestaurantFinder.post("/", {
-        name,
-        location,
-        price_range: priceRange,
-      });
-      console.log(response.data.data);
-      addRestaurants(response.data.data.restaurant);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+const Reviews = ({ reviews }) => {
   return (
-    <div className="mb-4">
-      <form action="">
-        <div className="form-row">
-          <div className="col">
-            <input
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              type="text"
-              className="form-control"
-              placeholder="name"
-            />
-          </div>
-          <div className="col">
-            <input
-              value={location}
-              onChange={(e) => setLocation(e.target.value)}
-              className="form-control"
-              type="text"
-              placeholder="location"
-            />
-          </div>
-          <div className="col">
-            <select
-              value={priceRange}
-              onChange={(e) => setPriceRange(e.target.value)}
-              className="custom-select my-1 mr-sm-2"
-            >
-              <option disabled>Price Range</option>
-              <option value="1">$</option>
-              <option value="2">$$</option>
-              <option value="3">$$$</option>
-              <option value="4">$$$$</option>
-              <option value="5">$$$$$</option>
-            </select>
-          </div>
-          <button
-            onClick={handleSubmit}
-            type="submit"
-            className="btn btn-primary"
+    <div className="row row-cols-3 mb-2">
+      {reviews.map((review) => {
+        return (
+          <div
+            key={review.id}
+            className="card text-white bg-primary mb-3 mr-4"
+            style={{ maxWidth: "30%" }}
           >
-            Add
-          </button>
+            <div className="card-header d-flex justify-content-between">
+              <span>{review.name}</span>
+              <span>
+                <StarRating rating={review.rating} />
+              </span>
+            </div>
+            <div className="card-body">
+              <p className="card-text">{review.review}</p>
+            </div>
+          </div>
+        );
+      })}
+      {/* <div
+        className="card text-white bg-primary mb-3 mr-4"
+        style={{ maxWidth: "30%" }}
+      >
+        <div className="card-header d-flex justify-content-between">
+          <span>Joan</span>
+          <span>
+            <StarRating rating={3} />
+          </span>
         </div>
-      </form>
+        <div className="card-body">
+          <p className="card-text">This restaurant was awesome</p>
+        </div>
+      </div>
+      <div
+        className="card text-white bg-primary mb-3 mr-4"
+        style={{ maxWidth: "30%" }}
+      >
+        <div className="card-header d-flex justify-content-between">
+          <span>Joan</span>
+          <span>
+            <StarRating rating={3} />
+          </span>
+        </div>
+        <div className="card-body">
+          <p className="card-text">This restaurant was awesome</p>
+        </div>
+      </div>
+      <div
+        className="card text-white bg-primary mb-3 mr-4"
+        style={{ maxWidth: "30%" }}
+      >
+        <div className="card-header d-flex justify-content-between">
+          <span>Joan</span>
+          <span>
+            <StarRating rating={3} />
+          </span>
+        </div>
+        <div className="card-body">
+          <p className="card-text">This restaurant was awesome</p>
+        </div>
+      </div>
+      <div
+        className="card text-white bg-primary mb-3 mr-4"
+        style={{ maxWidth: "30%" }}
+      >
+        <div className="card-header d-flex justify-content-between">
+          <span>Joan</span>
+          <span>
+            <StarRating rating={3} />
+          </span>
+        </div>
+        <div className="card-body">
+          <p className="card-text">This restaurant was awesome</p>
+        </div>
+      </div> */}
     </div>
   );
 };
 
-export default AddRestaurant;
+export default Reviews;
